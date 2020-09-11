275184e85018695478638b01dedd29d9c1f3ad45
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-11 13:09:36 +1200

    Create question table for database

---
+++ seeds/03-questions.js
@@ -0,0 +1,11 @@
+
+exports.seed = (knex) => {
+  // Deletes ALL existing entries
+  return knex('questions').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('questions').insert([
+        {id: 1, question: 'test question'},
+      ])
+    })
+}
