e75e8aa49e8d1782ff08304602497712b4a816e6
taty500summers <tatiana.bernon@gmail.com>
2020-11-10 10:06:20 +1300

    Change for heroku to deploy and build db correctly

--- knexfile.js
+++ knexfile.js
@@ -26,13 +26,9 @@ module.exports = {
     }
   },
 
-  production: {
+  roduction: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
